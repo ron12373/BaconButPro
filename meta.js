737b6e0806b9e7cbd35ef506936e8313ccb09b80
ron12373 <147504175+ron12373@users.noreply.github.com>
2024-07-24 10:53:45 +0700

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         Bypass Key System
+// @homepageURL  https://discord.gg/gFNAH7WNZj
+// @namespace    Best Bypassing
+// @version      1.8.5
+// @description  Bypass Arceus, Hydrogen, Delta, Trigon, Vegax, Codex, Linkvertise
+// @author       Bacon But Pro
+// @match        https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing*
+// @match        https://linkvertise.com/*/*
+// @match        https://socialwolvez.com/*
+// @match        https://rekonise.com/*
+// @match        https://mboost.me/a/*
+// @match        https://loot-link.com/s?*
+// @match        https://loot-links.com/s?*
+// @match        https://lootlink.org/s?*
+// @match        https://lootlinks.co/s?*
+// @match        https://gateway.platoboost.com/a/2569?id=*
+// @match        https://gateway.platoboost.com/a/8?id=*
+// @match        https://lootdest.info/s?*
+// @match        https://lootdest.org/s?*
+// @match        https://mobile.codex.lol/*
+// @match        https://pandadevelopment.net/getkey?*
+// @match        https://lootdest.com/s?*
+// @match        https://links-loot.com/s?*
+// @match        https://linksloot.net/s?*
+// @match        https://spdmteam.com/key-system*
+// @match        https://*/recaptcha/*
+// @match        https://*.hcaptcha.com/*hcaptcha-challenge*
+// @match        https://*.hcaptcha.com/*checkbox*
+// @match        https://*.hcaptcha.com/*captcha*
+// @match        https://bot-hosting.net/*
+// @match        *://trigonevo.com/getkey/?hwid=*
+// @match        https://work.ink/*/*
+// @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
+// @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require     https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/AutoGetCoin/Userscript.js
+// @require     https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/React.js
+// @require     https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/Main.js
+// @require     https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/Lootlabs.js
+// @require     https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/FastLootLabs.js
+
+// @run-at       document-start
+// @run-at       document-end
+// @grant        GM_xmlhttpRequest
+// @grant        GM_notification
+// @grant        GM_openInTab
+// @grant        unsafeWindow
+
+// @license      Bacon
+// @connect      linkvertise.com
+// @connect      short-jambo.com
+// @connect      api.codex.lol
+// @connect      cdn.jsdelivr.net
+// @connect      fluxteam.net
+// @connect      spdmteam.com
+// @connect      api-gateway.platoboost.com
+// @connect      api.valyse.best
+// @connect      api.keyrblx.com
+// @connect      pandadevelopment.net
+// @connect      hohohubv-ac90f67762c4.herokuapp.com
+// @connect      tsuo-script.xyz
+// @icon         https://cdn141.picsart.com/351217840073211.png
+// @downloadURL https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/baconbutpro.meta.js
+// @updateURL https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/baconbutpro.meta.js
+// ==/UserScript==
