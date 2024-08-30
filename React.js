465e6a391261d23766f5f96a731d08637ecb1f78
ron12373 <147504175+ron12373@users.noreply.github.com>
2024-08-30 20:18:36 +0700

    Update React.js

--- React.js
+++ React.js
@@ -1,18 +1,1 @@
-(function() {
-    'use strict';
-
-    // Get the current URL
-    let currentUrl = window.location.href;
-
-    // Check if the URL starts with the specified string
-    if (currentUrl.startsWith("https://trigonevo.com/getkey/?hwid=")) {
-        // Extract the HWID from the URL
-        let hwid = currentUrl.split("hwid=")[1];
-
-        // Construct the new URL
-        let newUrl = `https://pandadevelopment.net/getkey?service=trigon-evo&hwid=${hwid}&provider=linkvertise`;
-
-        // Redirect to the new URL
-        window.location.href = newUrl;
-    }
-})();
+function _0x1414(_0x11694d,_0x5b534d){const _0x1c2cdd=_0x1c2c();return _0x1414=function(_0x141438,_0x4cc63d){_0x141438=_0x141438-0xeb;let _0x5ea197=_0x1c2cdd[_0x141438];return _0x5ea197;},_0x1414(_0x11694d,_0x5b534d);}function _0x1c2c(){const _0x1dc959=['24215340skwqsl','348056tEYPeJ','2178432mAenim','59648kstFCu','123fdWJUv','location','hwid=','https://pandadevelopment.net/getkey?service=trigon-evo&hwid=','4115885xIiVMz','https://trigonevo.com/getkey/?hwid=','href','split','441875SNEVYw','4CPrZgA','10772604bCtOzI','105AWxaOE'];_0x1c2c=function(){return _0x1dc959;};return _0x1c2c();}(function(_0xb2d372,_0x1ef352){const _0x175b60=_0x1414,_0xe471f6=_0xb2d372();while(!![]){try{const _0x318624=parseInt(_0x175b60(0xeb))/0x1+-parseInt(_0x175b60(0xf2))/0x2*(-parseInt(_0x175b60(0xf3))/0x3)+parseInt(_0x175b60(0xec))/0x4*(parseInt(_0x175b60(0xf7))/0x5)+parseInt(_0x175b60(0xed))/0x6+-parseInt(_0x175b60(0xee))/0x7*(parseInt(_0x175b60(0xf0))/0x8)+-parseInt(_0x175b60(0xf1))/0x9+-parseInt(_0x175b60(0xef))/0xa;if(_0x318624===_0x1ef352)break;else _0xe471f6['push'](_0xe471f6['shift']());}catch(_0x4f9194){_0xe471f6['push'](_0xe471f6['shift']());}}}(_0x1c2c,0xec1ab),(function(){'use strict';const _0x19fe5d=_0x1414;let _0x5a5bb4=window[_0x19fe5d(0xf4)]['href'];if(_0x5a5bb4['startsWith'](_0x19fe5d(0xf8))){let _0x4cc5b9=_0x5a5bb4[_0x19fe5d(0xfa)](_0x19fe5d(0xf5))[0x1],_0x32bb7f=_0x19fe5d(0xf6)+_0x4cc5b9+'&provider=linkvertise';window[_0x19fe5d(0xf4)][_0x19fe5d(0xf9)]=_0x32bb7f;}}()));
