6c7af09c9d347df9685c7b659324c143e4daf198
BaconButPro <147504175+ron12373@users.noreply.github.com>
2024-10-30 21:46:25 +0700

    Update Bypass.user.js

--- Install-Userscript/Bypass.user.js
+++ Install-Userscript/Bypass.user.js
@@ -3,7 +3,7 @@
 // @name        Bypass Key System
 // @homepageURL https://discord.gg/gFNAH7WNZj
 // @namespace   https://greasyfork.org/en/scripts/493960-arceus-bypass
-// @version     2.6.9
+// @version     1.0
 // @description Bypass Arceus, Hydrogen, Delta, Trigon, Vegax, Codex, Linkvertise, panda, etc,...
 // @author      Bacon But Pro
 // @match       *://linkvertise.com/*/*
