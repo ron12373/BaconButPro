5e262225e786f3eb3c4c490dffba2d29d558d60a
BaconButPro <147504175+ron12373@users.noreply.github.com>
2025-07-11 09:58:51 +0700

    Create Bacon.user.js

---
+++ Bacon.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Bypass Link (Beta)
+// @version      1.5
+// @description  Still working on others link & and improving
+// @author       Bacon
+// @match        *://loot-link.com/s?*
+// @match        *://loot-links.com/s?*
+// @match        *://lootlink.org/s?*
+// @match        *://lootlinks.co/s?*
+// @match        *://lootdest.info/s?*
+// @match        *://lootdest.org/s?*
+// @match        *://lootdest.com/s?*
+// @match        *://links-loot.com/s?*
+// @match        *://linksloot.net/s?*
+// @match        *://87.106.44.40:6357/bypass.html?url=*
+// @grant        none
+// ==/UserScript==
