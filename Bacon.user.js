00446c920ed5e257e195c2076fd4baf6e76ca015
BaconButPro <147504175+ron12373@users.noreply.github.com>
2025-07-20 11:03:15 +0700

    Update Bacon.user.js

--- Bacon.user.js
+++ Bacon.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bypass Link (Beta)
-// @version      1.1.4
+// @version      1.1.5
 // @description  Still working on others link & and improving
 // @author       Bacon
 // @homepageURL  https://discord.gg/gFNAH7WNZj
@@ -35,6 +35,10 @@
 // @match        *://auth.platoboost.click/*
 // @match        *://auth.platoboost.net/*
 // @match        *://auth.platorelay.com/*
+// @match        *://flux.li/android/external/*
+// @match        *://bstlar.com/*
+// @match        *://mobile.codex.lol/*
+// @match        *://social-unlock.com/*
 // @match        *://bypass-linkv2.vercel.app/bypass.html?url=*
 // @require      https://cdn.rawgit.com/ricmoo/aes-js/e27b99df/index.js
 // @require      https://github.com/ron12373/BaconButPro/raw/main/Main.js
