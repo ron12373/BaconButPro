1cc28c0ae67d384392fc9a3bf61350a214f934e3
BaconButPro <147504175+ron12373@users.noreply.github.com>
2025-08-11 21:03:39 +0700

    Update Workink.user.js

--- Workink.user.js
+++ Workink.user.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name            Auto Work ink
-// @version         1.0
+// @version         1.1
 // @description     Work.ink + Timer Hooker (By Tiger 27)
-// @include         *
 // @require         https://greasyfork.org/scripts/372672-everything-hook/code/Everything-Hook.js?version=881251
 // @author          Buicon999
 // @match           https://work.ink/*
