fe5941d3cf976ccc00ae76bb6d31ff7a30271fd4
BaconButPro <147504175+ron12373@users.noreply.github.com>
2025-08-25 10:41:38 +0700

    Update Bacon.user.js

--- Bacon.user.js
+++ Bacon.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bacon Bypass Userscript
-// @version      2.1.4
+// @version      2.1.5
 // @description  Made by Bacon But Pro
 // @author       Bacon
 // @homepageURL  https://discord.gg/gFNAH7WNZj
